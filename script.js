cbf001a2c49382f04d0b6b1fe967bf56072c69f2
EcosEscenicos <146030280+EcosEscenicos@users.noreply.github.com>
2023-09-29 14:58:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-// Función para cargar las obras desde el archivo de texto en el servidor
-function cargarObras() {
-    // Aquí deberías tener código para cargar los datos desde el archivo de texto en el servidor
-    // Puedes utilizar Python para manejar esta parte del servidor
-    // Por simplicidad, en este ejemplo, utilizaremos datos simulados
-    const obras = [
-        ["Obra 1", "Autor 1", 5, "Drama", "Tipo 1", "2 horas", "Sinopsis 1", "autor1@example.com"],
-        ["Obra 2", "Autor 2", 4, "Comedia", "Tipo 2", "1.5 horas", "Sinopsis 2", "autor2@example.com"],
-        // Agrega más obras aquí...
-    ];
-    return obras;
-}
+// Datos de obras de teatro (simulados)
+const obras = [
+    ["Obra 1", "Autor 1", "5", "Drama", "2 horas", "Sinopsis 1", "autor1@example.com"],
+    ["Obra 2", "Autor 2", "4", "Comedia", "1.5 horas", "Sinopsis 2", "autor2@example.com"],
+    // Agrega más obras aquí...
+];
 
 // Función para mostrar las obras en la tabla
 function mostrarObras(obras) {
-    const listaObras = document.getElementById("listaObras");
-    listaObras.innerHTML = ""; // Limpiar la tabla
+    const tabla = document.querySelector(".tabla");
 
     obras.forEach(obra => {
-        const row = document.createElement("tr");
+        const fila = document.createElement("div");
+        fila.classList.add("fila");
+
         obra.forEach(dato => {
-            const cell = document.createElement("td");
-            cell.textContent = dato;
-            row.appendChild(cell);
+            const columna = document.createElement("div");
+            columna.classList.add("columna");
+            columna.textContent = dato;
+            fila.appendChild(columna);
         });
-        listaObras.appendChild(row);
+
+        tabla.appendChild(fila);
     });
 }
 
@@ -31,19 +28,26 @@ function mostrarObras(obras) {
 function buscarObras() {
     const campo = document.getElementById("campo").value;
     const busqueda = document.getElementById("busqueda").value.toLowerCase();
-    const obras = cargarObras();
-    const resultados = obras.filter(obra => obra[getIndiceCampo(campo)].toLowerCase().includes(busqueda));
+
+    const resultados = obras.filter(obra => {
+        return String(obra[obtenerIndiceCampo(campo)]).toLowerCase().includes(busqueda);
+    });
+
+    // Eliminar las filas actuales de la tabla
+    const tabla = document.querySelector(".tabla");
+    while (tabla.firstChild) {
+        tabla.removeChild(tabla.firstChild);
+    }
+
+    // Mostrar los resultados de la búsqueda
     mostrarObras(resultados);
 }
 
 // Función para obtener el índice del campo seleccionado en el formulario
-function getIndiceCampo(campo) {
-    const campos = ["titulo", "autor", "personajes", "genero", "tipo", "duracion", "sinopsis", "contacto"];
+function obtenerIndiceCampo(campo) {
+    const campos = ["titulo", "autor", "personajes", "tipo", "duracion", "sinopsis", "contacto"];
     return campos.indexOf(campo);
 }
 
-// Cargar todas las obras al cargar la página
-document.addEventListener("DOMContentLoaded", () => {
-    const obras = cargarObras();
-    mostrarObras(obras);
-});
+// Mostrar todas las obras al cargar la página
+mostrarObras(obras);
